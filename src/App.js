80d805546d127ae31ebb576f6873b62e7fcedf56
Marcin Szote <Marcinszoot@gmail.com>
2022-11-29 15:13:23 +0000

    Counter mini app - learning use state hook.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,264 @@
 import './App.css';
-import {User} from './User.js' ;
+import {useState} from "react";
 
 
 
 function App() {
-  const users = [
-  { name: "Marcin", age : 32 },
-  { name: "Edd", age :20},
-  { name: "Jesse", age :69},
-  ]
+
+  const[count,setCount] = useState(0)
+
   return (
-    <div className='App'> 
-    {users.map((user) =>{
-      return <User name={user.name} age={user.age}/>
-    })}
- </div>
-  );
-}
+  <div className='App'>
+    <h2>Simple Counter App</h2>
+    <h1>{count}</h1>
+    <button onClick={()=>setCount(count +1)}>Incrase</button>
+    <button onClick={()=> setCount(count -1)}>Decrase</button>
+    <button onClick={()=>setCount(0)}>Reset</button>
+  </div>
+)}
 
 
 
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import './App.css';
+// import {User} from './User.js' ;
+// import {Planets} from './User.js' ;
+// import {useState} from "react";
+
+
+
+// function App() {
+
+//   const [changeColor,setChangeColor] = useState('black')
+
+//   const toggleColor = () => {
+//     setChangeColor(changeColor === 'black' ? "red" : "black")
+//   }
+
+//   return (
+//   <div className='App'>
+//     <button onClick={toggleColor}>Change color!</button>
+//    <h1 style={{color: changeColor}}>Hi my name is Marcin!</h1>
+//   </div>
+// )}
+
+
+
+
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import './App.css';
+// import {User} from './User.js' ;
+// import {Planets} from './User.js' ;
+// import {useState} from "react";
+
+
+
+// function App() {
+
+// const [input,setInput] = useState('');
+
+// const handleInputChange = (event) => {
+//   setInput(event.target.value)
+// }
+
+
+//   return (
+//   <div className='App'>
+//   <input type='text' onChange={handleInputChange}/>
+//   <br></br>
+//   {input}
+//   </div>
+// )}
+
+
+
+
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import './App.css';
+// import {User} from './User.js' ;
+// import {Planets} from './User.js' ;
+// import {useState} from "react";
+
+
+
+// function App() {
+//   const [age,setAge] = useState(31)
+//   const incraseAge = () => {
+//       setAge(age+1)
+//   }
+//   return <div className='App'>{age}<button onClick={incraseAge}>Incrase Age</button></div>
+// }
+
+
+
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import './App.css';
+// import {User} from './User.js' ;
+// import {Planets} from './User.js' ;
+
+
+
+// function App() {
+
+
+//   const users = [
+//   { name: "Marcin", age : 32 },
+//   { name: "Edd", age :20},
+//   { name: "Jesse", age :69},
+//   ]
+
+//   const planets =[
+//     { name: "Mars" , isGasPlanet : false},
+//     { name: "Earth" , isGasPlanet : false},
+//     { name: "Jupiter" , isGasPlanet : true},
+//     { name: "Venus" , isGasPlanet : false},
+//     { name: "Neptune" , isGasPlanet : true},
+//     { name: "Uranus" , isGasPlanet : true},
+//   ]
+
+
+
+//   return (
+//     <div className='App'>
+//       <div>
+//         {users.map((user) =>{
+//         return <User name={user.name} age={user.age}/>
+//         })}
+//     </div>
+//     <br></br>
+//     <br></br>
+//     <br></br>
+//     <br></br>
+//     <div>
+//       {planets.map((planet)=> planet.isGasPlanet && <Planets name={planet.name}/>
+    
+//       )}
+//     </div>
+//  </div>
+
+//   );
+// }
+
+
+
+// export default App;
 
 
 
--- src/User.js
+++ src/User.js
@@ -1,2 +1,9 @@
 export const User = (props) => <div>Hi {props.name}, you are {props.age} old !</div> ;
 
+export const Planets = (prompt) => {
+    return <div>{prompt.name}</div>
+  }
+
+
+
+
